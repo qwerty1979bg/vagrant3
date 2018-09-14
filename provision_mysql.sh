f29883a1c32b89ee7c113bebc46c7422b49db913
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-09-14 16:43:12 +0200

    Delete provision_mysql.sh

--- provision_mysql.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export DEBIAN_FRONTEND=noninteractive
-export APTARGS="-qq -o=Dpkg::Use-Pty=0"
-
-apt-get update ${APTARGS}
-apt-get install -y mysql-server ${APTARGS}
-apt-get clean ${APTARGS}
